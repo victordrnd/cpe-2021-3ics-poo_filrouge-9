ac4f7a4e86073c91d19d43f1223713283228a596
Victor Durand <vic20016@gmail.com>
2020-11-27 13:59:59 +0100

    fix: get Coord on Itinerary fnct

--- src/main/java/filRouge/checkersGameModel/PawnModel.java
+++ src/main/java/filRouge/checkersGameModel/PawnModel.java
@@ -45,40 +45,20 @@ public class PawnModel implements PieceModel {
 	@Override
 	public List<Coord> getCoordsOnItinerary(Coord targetCoord) {
 		List<Coord> coordsOnItinerary = new ArrayList<Coord>();
-		// int hCount = (targetCoord.getColonne() - this.coord.getColonne()); // Droite
-		// +1 - Gauche -1
-		// // hCount *= pieceColor.equals(PieceSquareColor.WHITE) ? -1 : 1;
-		// int vCount = (targetCoord.getLigne() - this.coord.getLigne());
-		// // vCount *= pieceColor.equals(PieceSquareColor.WHITE) ? -1 : 1;
-		// boolean droite = hCount > 0;
-		// for (int i = this.coord.getLigne() + 1; i < Math.abs(vCount); i++) {
-		// Coord coord;
-		// int col_to_add = (i * hCount);
-		// char col = (char) (this.coord.getColonne() + (char) col_to_add);
-		// coord = new Coord(col, i);
-		// coordsOnItinerary.add(coord);
-		// }
-		// int factor = pieceColor.equals(PieceSquareColor.WHITE) ? 1 : -1;
-		if (Math.abs(this.coord.getLigne() - targetCoord.getLigne()) > 1) {
-			Coord coord;
-			if (pieceColor.equals(PieceSquareColor.WHITE)) {
-				boolean droite = this.coord.getColonne() < targetCoord.getColonne();
+		int factor = pieceColor.equals(PieceSquareColor.WHITE) ? 1 : -1;
+		int diagonal_move_count = Math.abs(this.coord.getLigne() - targetCoord.getLigne());
+		if(diagonal_move_count > 1){
+			for(int i = 1; i < diagonal_move_count; i++){
+				Coord coord;
+				boolean droite = ((this.coord.getColonne() < targetCoord.getColonne()) ^ (factor== -1));
 				if (droite) {
-					coord = new Coord((char) (this.getCoord().getColonne() + 1), this.getCoord().getLigne() + 1);
+					coord = new Coord((char) (this.getCoord().getColonne() + (factor*i)), this.getCoord().getLigne() + (factor*i));
 				} else {
-					coord = new Coord((char) (this.getCoord().getColonne() - 1), this.getCoord().getLigne() + 1);
+					coord = new Coord((char) (this.getCoord().getColonne() - (factor*i)), this.getCoord().getLigne() + (factor*i) );
 				}
-			} else {
-				boolean droite = this.coord.getColonne() > targetCoord.getColonne();
-				if (droite) {
-					coord = new Coord((char) (this.getCoord().getColonne() - 1), this.getCoord().getLigne() - 1 );
-				} else {
-					coord = new Coord((char) (this.getCoord().getColonne() + 1), this.getCoord().getLigne() - 1);
-				}
+				coordsOnItinerary.add(coord);
 			}
-			coordsOnItinerary.add(coord);
 		}
-
 		return coordsOnItinerary;
 	}
 
